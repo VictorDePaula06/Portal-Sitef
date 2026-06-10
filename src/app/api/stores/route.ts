85f936ef032aa2d4526a40fd0bbbd30545e69efb
Victor <victor@globaltera.com.br>
2026-06-10 15:54:14 -0300

    Fix missing account id causing edit to fail

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -18,6 +18,7 @@ export async function GET(request: Request) {
       include: {
         account: {
           select: {
+            id: true,
             email: true,
             password: true
           }
